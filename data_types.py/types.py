83e49dd00a7f7ce778207ce600bd9154170a292a
Abhijeet R Sharma <97837777+abhii8086@users.noreply.github.com>
2024-05-12 16:53:15 +0530

    Add files via upload

---
+++ data_types.py/types.py
@@ -0,0 +1,63 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# String
+name = "Abhi" 
+print(name)
+
+# numeric
+# integer 
+a = 5  
+# float 
+b = 5.5
+# complex
+c = 1j
+print(a,b,c)
+
+#sequence 
+#list
+vegetable = ["aalu","matar","bagan"]
+print(vegetable)
+#tuple
+fruits = ("apple","banana","cherry","mango")
+print(fruits)
+#range
+x = range(10)
+print(x)
+
+#Mapping type
+student_reg = {"A":"Abhijeet","B":"Bijay","C":"Cekhar"}
+print(student_reg)
+
+#Set type
+Number_set ={1,2,3,4,5,6,6}
+print(Number_set)
+
+nset = frozenset({"a","b","c"})
+print(nset)
+
+#boolean
+x = True
+y = False
+print(x,y)
+
+#binary type
+x = bytearray(5)
+print(x)
+
+x = b"ngw"
+print(type(x))
+
+y = memoryview(bytes(5))
+print(type(y))
+
+# none
+z = None
+print(type(z))
+
