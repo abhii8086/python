61df668ca849b87d6abd60b5e64bff83fafaf73a
Abhijeet R Sharma <97837777+abhii8086@users.noreply.github.com>
2024-06-13 23:29:18 +0530

    Add files via upload

---
+++ evenOdd.py
@@ -0,0 +1,9 @@
+def evenOdd(num):
+    if(num%2==0):
+        print(num,"is even")
+    else:
+        print(num,"is odd")
+
+while (1>0):        
+    num = int(input("enter a number: "))
+    evenOdd(num)
